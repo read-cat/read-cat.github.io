2d5b3e7283432e8351cb460c83dbb4bc7456fd19
read-cat <ljy1345514736@163.com>
2024-04-15 20:35:22 +0800

    deploy

--- docs/assets/index-B4Bx5jj_.js
+++
@@ -1,1 +0,0 @@
-import{K as c}from"./index-CWqZ_Ewe.js";const e={};function n(r,t){return null}const o=c(e,[["render",n],["__scopeId","data-v-808b76bc"]]);export{o as default};
---
+++ make.cjs
@@ -0,0 +1,4 @@
+
+const fs = require('fs');
+
+fs.copyFileSync('docs/index.html', 'docs/404.html');
